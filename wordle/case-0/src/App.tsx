700b7a74e6eab6e1b6f07143c34717ae152c6cdc
Mariano <macacozzz@hotmail.com>
2022-11-22 10:10:48 -0300

    resolucion de case-1

--- wordle/case-0/src/App.tsx
+++ wordle/case-0/src/App.tsx
@@ -1,11 +1,13 @@
 import {KeyboardEvent, useCallback, useEffect, useState} from "react";
 import Confetti from "react-confetti";
 
+import api from "./api";
+
 function App() {
-  const answer = "RIGHT";
+  const [answer, setAnswer] = useState("RIGHT");
   const [turn, setTurn] = useState<number>(0);
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
-  const [status, setStatus] = useState<"playing" | "finished">("playing");
+  const [status, setStatus] = useState<"playing" | "finished" | "confetti">("playing");
   const [words, setWords] = useState<string[][]>(() =>
     Array.from({length: 6}, () => new Array(5).fill("")),
   );
@@ -16,102 +18,103 @@ function App() {
     }
   }, [status]);
 
-  function restartGame() {
+  const restartGame = async () => {
     console.log("restarting game");
     setTurn(0);
     setWords(() => Array.from({length: 6}, () => new Array(5).fill("")));
+    const newAnswer = await api.word.random();
+
+    setAnswer(newAnswer);
+
     setStatus("playing");
     setShowConfetti(false);
-  }
+  };
 
-  const handleKeyDown = useCallback(
-    (event: KeyboardEvent) => {
-      //alert("hola");
-      //console.log("Key down: " + event.key);
-      switch (event.key) {
-        case "Enter": {
-          if (status === "finished") {
-            restartGame();
-          }
-          if (words[turn].join("") === answer) {
-            setStatus("finished");
+  useEffect(() => {
+    if (!showConfetti) {
+      //      getNewAnswer();
+    }
+  }, [showConfetti]);
 
-            return;
-          }
+  const getNewAnswer = async () => {
+    const newAnswer = await api.word.random();
 
-          const filledLetters = words[turn].filter((letter) => letter !== "");
+    setAnswer(newAnswer);
+  };
 
-          if (filledLetters.length === words[turn].length) {
-            setTurn((turn) => turn + 1);
-          }
+  const handleKeyDown = (event: KeyboardEvent) => {
+    //useCallback(
+    //alert("hola");
+    console.log("Llamando a handlekeydown");
+    if (status === "finished") {
+      restartGame();
+
+      return;
+    }
+    switch (event.key) {
+      case "Enter": {
+        if (words[turn].join("") === answer) {
+          setStatus("finished");
 
           return;
         }
-        case "Backspace": {
-          let firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
 
-          if (firstEmptyIndex === -1) {
-            firstEmptyIndex = words[turn].length;
+        const filledLetters = words[turn].filter((letter) => letter !== "");
+
+        if (filledLetters.length === words[turn].length) {
+          setTurn((turn) => turn + 1);
+        }
+
+        return;
+      }
+      case "Backspace": {
+        let firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
+
+        if (firstEmptyIndex === -1) {
+          firstEmptyIndex = words[turn].length;
+        }
+
+        words[turn][firstEmptyIndex - 1] = "";
+
+        setWords(words.slice());
+
+        return;
+      }
+      default: {
+        if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
+          const firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
+
+          if (firstEmptyIndex === -1) return;
+
+          words[turn][firstEmptyIndex] = event.key.toUpperCase();
+          if (firstEmptyIndex === words[turn].length) {
+            setTurn(turn + 1);
           }
-
-          words[turn][firstEmptyIndex - 1] = "";
 
           setWords(words.slice());
 
           return;
         }
-        default: {
-          if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
-            const firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
-
-            if (firstEmptyIndex === -1) return;
-
-            words[turn][firstEmptyIndex] = event.key.toUpperCase();
-            if (firstEmptyIndex === words[turn].length) {
-              setTurn(turn + 1);
-            }
-
-            setWords(words.slice());
-
-            return;
-          }
-        }
       }
-    },
-    [turn, words, answer],
-  );
+    }
+  };
+  //  [turn, words, answer],
+  // );
+
+  if (!answer) {
+    return <div>Cargando respuesta</div>;
+  }
 
   return (
     <>
       <main className="board" tabIndex={-1} onKeyDown={(e) => handleKeyDown(e)}>
-        {showConfetti ? (
-          <>
-            <Confetti
-              colors={["#73ACDF", "#FFFFFF"]}
-              drawShape={(ctx) => {
-                ctx.beginPath();
-                for (let i = 0; i < 22; i++) {
-                  const angle = 0.35 * i;
-                  const x = (0.2 + 1.5 * angle) * Math.cos(angle);
-                  const y = (0.2 + 1.5 * angle) * Math.sin(angle);
-
-                  ctx.lineTo(x, y);
-                }
-                ctx.stroke();
-                ctx.closePath();
-              }}
-              numberOfPieces={200}
-            />
-            <h1>Enter para jugar de nuevo</h1>
-          </>
-        ) : null}
         {words.map((word, wordIndex) => (
           <section key={wordIndex} className="word">
             {word.map((letter, letterIndex) => {
               const isCorrect = answer.charAt(letterIndex) === letter.toUpperCase();
               const isPresent =
                 letter &&
-                wordIndex < turn &&
+                // wordIndex < turn &&
                 letter !== answer[letterIndex] &&
                 answer.includes(letter);
 
@@ -127,6 +130,29 @@ function App() {
           </section>
         ))}
       </main>
+      {showConfetti ? (
+        <>
+          <Confetti
+            colors={["#73ACDF", "#FFFFFF"]}
+            drawShape={(ctx) => {
+              ctx.beginPath();
+              for (let i = 0; i < 22; i++) {
+                const angle = 0.35 * i;
+                const x = (0.2 + 1.5 * angle) * Math.cos(angle);
+                const y = (0.2 + 1.5 * angle) * Math.sin(angle);
+
+                ctx.lineTo(x, y);
+              }
+              ctx.stroke();
+              ctx.closePath();
+            }}
+            numberOfPieces={200}
+          />
+          <div className="instrucciones">
+            <h1>Enter para jugar de nuevo</h1>
+          </div>
+        </>
+      ) : null}
     </>
   );
 }
--- wordle/case-0/src/api.ts
+++ wordle/case-0/src/api.ts
@@ -1,9 +1,13 @@
 const WORDS = ["WRONG", "RIGHT", "WORLD", "WORMS", "GONCY", "BELEN"];
+const min = 0;
+const max = WORDS.length - 1;
 
 const api = {
   word: {
     random: (): Promise<string> => // CANDIDATE
-      new Promise((resolve) => setTimeout(() => resolve("RIGHT"), 1000)),
+      new Promise((resolve) =>
+        setTimeout(() => resolve(WORDS[Math.floor(Math.random() * (max - min + 1) + min)]), 1000),
+      ),
   },
 };
 
